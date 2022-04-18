e57334905f697c87530525523cc11ee671dcb76f
Allegui <32678906+Allegui@users.noreply.github.com>
2022-04-18 15:15:55 +0200

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -12,7 +12,7 @@ SK_ID_CURR_test_X = pd.read_csv('SK_ID_CURR_test_X.csv')
 SK_ID_CURR_test_X = SK_ID_CURR_test_X.drop(["Unnamed: 0"], axis=1)
 
    
-def API_prediction(lien,numero_client):
+def requests(lien,numero_client):
     data_df = test_X.loc[test_X.index==np.asscalar(SK_ID_CURR_test_X.loc[SK_ID_CURR_test_X['SK_ID_CURR']==numero_client].index),:]
     resultat = float(xgb_cl_undersampling.predict_proba(data_df)[0][1])
     return resultat
